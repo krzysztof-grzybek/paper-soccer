895f5bd8d002ac6d6f6f43bb318502eae25f4b57
Krzysztof Grzybek <krzysztof.grzybek89@gmail.com>
2019-03-18 21:55:31 +0100

    update heroku port

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 const url = process.env.NODE_ENV === 'production'
-  ? 'https://paper-soccer-be.herokuapp.com'
+  ? 'https://paper-soccer-be.herokuapp.com:8312'
   : 'https://localhost:3001';
 
 const config = {
