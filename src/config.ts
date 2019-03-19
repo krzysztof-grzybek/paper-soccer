0e27587974ef7f38bfca7a456bc15bc9776579e6
Krzysztof Grzybek <krzysztof.grzybek89@gmail.com>
2019-03-19 19:00:20 +0100

    fix front end build

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 const url = process.env.NODE_ENV === 'production'
-  ? 'https://paper-soccer-be.herokuapp.com:28512'
+  ? 'https://paper-soccer-be.herokuapp.com'
   : 'https://localhost:3001';
 
 const config = {
